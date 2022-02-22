28dc6cc3266231f7e71c019b3f76b98098ae6eab
Greg Abram <gda@tacc.utexas.edu>
2022-02-22 13:52:20 -0700

    closer...

--- start-container.sh
+++ start-container.sh
@@ -1,8 +1,35 @@
 #! /bin/bash
 
-bash ./setup-container.sh
-
+mkdir -p db
 mongod --port 1336 --dbpath db >> mongo.out 2>&1  &
 sleep 2
+
+echo "setup start"
+
+project=/ClimatePrisms.Content
+
+for i in $project/static/* ; do
+  lcl=`echo $i | sed "s?\$project/??"`
+  if [[ -e $lcl ]] ; then
+    echo removing $lcl
+    rm $lcl
+  fi
+  ln -s $i static
+done
+
+python3 create_homepage.py $project
+
+cd static
+
+for ss in $project/*xlsx ; do
+  python3 ../tools/fromExcel $ss
+done
+
+cd ..
+
+python3 ./tools/fillers.py $project
+
+echo "setup done"
+
 python3 server.py 1337 
 
